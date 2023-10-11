145c86ab6d4794e1be14cabede2a1af46f7cd720
mathi27 <mathiyuvarajan.dev@gmail.com>
2023-10-11 14:28:02 +0530

    bug fixes and added features

--- quiz.js
+++ quiz.js
@@ -1,6 +1,6 @@
 import questions from './question.js';
 
-  //shuffle the question pattern
+//shuffle the question pattern
 function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
@@ -13,13 +13,9 @@ let score = 0;
 let timer;
 let userName = null; // Initialize userName as null
 let hasClickedStartButton = false;
-let userInteracted = false; 
+let userInteracted = false;
+let hasPlayedCompletionSound = false;  
 
-//clear cache 
-function clearUserNameFromLocalStorage(){
-    localStorage.removeItem("userName");
-    userName = null;
-}
 
 
 
@@ -32,7 +28,9 @@ document.addEventListener("DOMContentLoaded", () => {
     const resultDisplay = document.getElementById("result");
     const optionsContainer = document.getElementById("options-container");
     const buttonClickSound = document.getElementById("buttonClickSound");
-    const timerCompletionSound = document.getElementById("bellSound");
+    const timerCompletionSound = document.getElementById("quizOver");
+    const kovilMani = document.getElementById("kovil-mani");
+    const wrongOptionSelected = document.getElementById("worngOption");
     const restartButton = document.getElementById("restart-button");
     const collectDataButton = document.getElementById("collect-data-button")
     const userNameInput = document.getElementById("user-name-input");
@@ -40,15 +38,24 @@ document.addEventListener("DOMContentLoaded", () => {
     const inputContainer = document.getElementById("input-container");
 
 
+// Function to clear the local storage 
+function clearLocalStorage() {
+    localStorage.removeItem("userName");
+    localStorage.removeItem("phoneNumber");
+}
+
+
+
     // Data collection:
-    collectDataButton.addEventListener("click",()=>{
-        const userName = userNameInput.value;
+    collectDataButton.addEventListener("click", () => {
+        userName = userNameInput.value;
         const phoneNumber = phoneNumberInput.value.trim();
 
-        if(!userName || !phoneNumber){
+        if (!userName || !phoneNumber) {
             alert("Please Enter both Name and Phone Number");
             return;
         }
+        
         // Validation for Username
         if (!/^[A-Za-z]+$/.test(userName)) {
             alert("User Name should contain only letters");
@@ -59,87 +66,90 @@ document.addEventListener("DOMContentLoaded", () => {
             alert("Phone Number should contain only numbers");
             return;
         }
-    
+        if (!/^\d{10}$/.test(phoneNumber)) {
+            alert("Phone Number should contain exactly 10 numbers");
+            return;
+        }
+        
+
         // store user name and phone number in local storage.
-        localStorage.setItem("userName",userName);
-        localStorage.setItem("phoneNumber",phoneNumber);
+        localStorage.setItem("userName", userName);
+        localStorage.setItem("phoneNumber", phoneNumber);
+
         console.log("Starting QUIZ");
+
         startScreen.style.display = "none";
         quizScreen.style.display = "block";
+
         startQuiz();
+        // to display this in screen (welcome, [UserName])
         document.getElementById("user-name").textContent = userName;
-      
-    
+ 
+
     });
-    
+
 
     document.addEventListener("click", () => {
         userInteracted = true;
     });
 
-     restartButton.addEventListener("click",()=>{
-        localStorage.removeItem("userName");
-        userName = null;
+    restartButton.addEventListener("click", () => {
+        clearLocalStorage();
 
         currentQuestion = 0;
         score = 0;
-        location.reload();          
+        location.reload();
+        // startQuiz();
+
     });
 
-  
-    
-     
-   
+
+
 
     function stopAudio() {
         timerCompletionSound.pause();
         timerCompletionSound.currentTime = 0;
     }
-    if(timerCompletionSound){
+    if (timerCompletionSound) {
         timerCompletionSound.addEventListener("ended", stopAudio);
     }
 
-    
+
     // Check if the user's name is already in localStorage
     const storedUserName = localStorage.getItem("userName");
     if (storedUserName) {
         userName = storedUserName;
-        hasClickedStartButton = true; // made the flag set true.
+        hasClickedStartButton = true;
         startScreen.style.display = "none";
         quizScreen.style.display = "block";
         document.getElementById("user-name").textContent = userName;
         startQuiz();
+    }else{
+        userName = userNameInput.value;
     }
     // THis start button is initial page UI button.
     startButton.addEventListener("click", () => {
-        buttonClickSound.play(); 
+        buttonClickSound.play();
         if (!hasClickedStartButton) {
-           startButton.style.display = "none";
-            
+            startButton.style.display = "none";
 
-           //Enabling the Input
-           inputContainer.style.display = "none";
-           inputContainer.style.display = "block";
-           
-            
+
+            //Enabling the Input
+            inputContainer.style.display = "none";
+            inputContainer.style.display = "block";
+
+
         }
-        // startScreen.style.display = "none";
-        // quizScreen.style.display = "block";
-        // document.getElementById("user-name").textContent = userName;
-        // startQuiz();
+        
 
-      
+
     });
 
 
-    function startQuiz() {
-        // Remove Exsisting Local Storage sessions.
-        localStorage.removeItem("userName");
-        localStorage.removeItem("phoneNumber");
-        console.log("Exsisting sessions are removed.")
+    function startQuiz() {    
         shuffleArray(questions);
-        
         document.getElementById("user-name").textContent = userName;
+        console.log(` --------------${userName}------------`);
         displayQuestion();
         timer = setInterval(updateTimer, 1000);
     }
@@ -154,7 +164,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 const optionButton = document.createElement("button");
                 optionButton.textContent = option;
                 optionButton.classList.add("options-button");
-                optionButton.addEventListener("click", () => checkAnswer(option,questionData.answer));
+                optionButton.addEventListener("click", () => checkAnswer(option, questionData.answer));
                 optionsContainer.appendChild(optionButton);
             });
         } else {
@@ -167,6 +177,17 @@ document.addEventListener("DOMContentLoaded", () => {
             // need to send this data to node.js server if i want.
         }
     }
+   
+    function playSound(isCorrect){
+        const correctSound = new Audio('./audio/hindu-bell1.mp3');
+        const wrongSound = new Audio('./audio/wrongOption.mp3');
+
+        if(isCorrect){
+            correctSound.play();
+        }else{
+            wrongSound.play();
+        }
+    }
 
     function updateTimer() {
         let timeLeft = parseInt(timerDisplay.textContent);
@@ -175,11 +196,12 @@ document.addEventListener("DOMContentLoaded", () => {
             resultDisplay.style.display = "block";
             resultDisplay.innerHTML = `Time's up, ${userName}! Your score: ${score} `;
             // Completion Sound;
-            if(userInteracted){
+            if (userInteracted && !hasPlayedCompletionSound) {
                 timerCompletionSound.play();
+                hasPlayedCompletionSound = true;
 
             }
-            if(score > 30){
+            if (score > 30) {
                 resultDisplay.innerHTML += `You are eligible for a Crackers!🧨`;
 
             }
@@ -191,35 +213,18 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     }
 
-    // BUG : API Request is not hittting
-    function hitApiRequest(){
-        const apiURL = "https://jsonplaceholder.typicode.com/users";
-        
-        fetch(apiUrl)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return response.json();  
-    })
-    .then(data => {
-    
-      console.log('API response data:', data);
-     })
-    .catch(error => {
-      console.error('API request failed:', error);
-    });
-        
-
-    }
     function checkAnswer(selectedOption) {
         // console.log(`Selected Option: ${selectedOption}, Correct Answer: ${correctAnswer}`);
         const correctAnswer = questions[currentQuestion].answer;
-        buttonClickSound.play();
+        // buttonClickSound.play();
 
-        if(selectedOption === correctAnswer){
+        if (selectedOption === correctAnswer) {
             score++;
+            playSound(true);
             console.log(`Score updated: ${score}`);
+        }else{
+            playSound(false);
+
         }
 
         currentQuestion++;
@@ -228,8 +233,8 @@ document.addEventListener("DOMContentLoaded", () => {
         //Update the score display on Menu :
         const scoreDisplay = document.getElementById("score-display");
         scoreDisplay.textContent = `Score: ${score}`;
-      
+
     }
 
-    // startQuiz();
+    
 });
