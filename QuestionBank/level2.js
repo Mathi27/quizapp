bd2e2947cf86efbdefa39933e7f0b4470d08661e
mathi27 <mathiyuvarajan.dev@gmail.com>
2024-01-02 13:52:37 +0530

    lvl 3

--- QuestionBank/level2.js
+++ QuestionBank/level2.js
@@ -1,9 +1,349 @@
 const level2Questions = [
-    {
-      question: "Yet to load level 2 Question",
-      options: ["0", "0", "0", "0"],
-      answer: "B",
-    },
+  {
+    question: "2 less than 468?",
+    options: ["467", "457", "458", "461", "466"],
+    answer: "466",
+  },
+  {
+    question: "2 less than 65?",
+    options: ["64", "57", "54", "63"],
+    answer: "63",
+  },
+  {
+    question: "2 less than 38?",
+    options: ["36", "34", "30", "33"],
+    answer: "33",
+  },
+  {
+    question: "2 less than 114?",
+    options: ["112", "110", "103", "108"],
+    answer: "108",
+  },
+  {
+    question: "2 less than 20?",
+    options: ["18", "16", "12", "14"],
+    answer: "14",
+  },
+  {
+    question: "2 less than 405?",
+    options: ["403", "400", "397", "395"],
+    answer: "403",
+  },
+  {
+    question: "2 less than 430?",
+    options: ["428", "427", "423", "425"],
+    answer: "425",
+  },
+  {
+    question: "2 less than 254?",
+    options: ["252", "251", "246", "249"],
+    answer: "249",
+  },
+  {
+    question: "2 less than 190?",
+    options: ["188", "187", "184", "182"],
+    answer: "184",
+  },
+  {
+    question: "2 less than 162?",
+    options: ["160", "153", "158", "160"],
+    answer: "160",
+  },
+  {
+    question: "2 less than 56?",
+    options: ["54", "49", "46", "54"],
+    answer: "54",
+  },
+  {
+    question: "2 less than 51?",
+    options: ["49", "43", "47", "49"],
+    answer: "49",
+  },
+  {
+    question: "2 less than 818?",
+    options: ["816", "811", "807", "816"],
+    answer: "816",
+  },
+  {
+    question: "2 less than 789?",
+    options: ["787", "778", "779", "787"],
+    answer: "787",
+  },
+  {
+    question: "2 less than 130?",
+    options: ["128", "121", "122", "128"],
+    answer: "128",
+  },
+  {
+    question: "2 less than 658?",
+    options: ["656", "648", "647", "656"],
+    answer: "656",
+  },
+  {
+    question: "2 less than 87?",
+    options: ["85", "79", "85"],
+    answer: "85",
+  },
+  {
+    question: "2 less than 842?",
+    options: ["840", "837", "831", "836"],
+    answer: "836",
+  },
+  {
+    question: "2 less than 837?",
+    options: ["835", "834", "833", "832"],
+    answer: "832",
+  },
+  {
+    question: "2 less than 640?",
+    options: ["638", "635", "631", "634"],
+    answer: "634",
+  },
+  {
+    question: "2 less than 712?",
+    options: ["710", "708", "715", "706"],
+    answer: "710",
+  },
+  {
+    question: "2 less than 96?",
+    options: ["94", "98", "91", "92"],
+    answer: "94",
+  },
+  {
+    question: "2 less than 53?",
+    options: ["51", "56", "49", "50"],
+    answer: "51",
+  },
+  {
+    question: "2 less than 123?",
+    options: ["121", "118", "124", "120"],
+    answer: "121",
+  },
+  {
+    question: "2 less than 30?",
+    options: ["28", "32", "26", "29"],
+    answer: "28",
+  },
+  {
+    question: "2 less than 567?",
+    options: ["565", "560", "570", "563"],
+    answer: "565",
+  },
+  {
+    question: "2 less than 402?",
+    options: ["400", "405", "398", "403"],
+    answer: "400",
+  },
+  {
+    question: "2 less than 189?",
+    options: ["187", "192", "183", "188"],
+    answer: "187",
+  },
+  {
+    question: "2 less than 277?",
+    options: ["275", "270", "279", "273"],
+    answer: "275",
+  },
+  {
+    question: "2 less than 145?",
+    options: ["143", "150", "140", "144"],
+    answer: "143",
+  },
+  {
+    question: "2 less than 63?",
+    options: ["61", "66", "59", "62"],
+    answer: "61",
+  },
+  {
+    question: "2 less than 42?",
+    options: ["40", "44", "38", "41"],
+    answer: "40",
+  },
+  {
+    question: "2 less than 928?",
+    options: ["926", "922", "932", "925"],
+    answer: "926",
+  },
+  {
+    question: "2 less than 801?",
+    options: ["799", "805", "797", "802"],
+    answer: "799",
+  },
+  {
+    question: "2 less than 175?",
+    options: ["173", "180", "170", "174"],
+    answer: "173",
+  },
+  {
+    question: "2 less than 764?",
+    options: ["762", "760", "770", "763"],
+    answer: "762",
+  },
+  {
+    question: "2 less than 94?",
+    options: ["92", "98", "90", "93"],
+    answer: "92",
+  },
+  {
+    question: "2 less than 572?",
+    options: ["570", "568", "578", "571"],
+    answer: "570",
+  },
+  {
+    question: "2 less than 209?",
+    options: ["207", "212", "205", "210"],
+    answer: "207",
+  },
+  {
+    question: "2 less than 740?",
+    options: ["738", "736", "746", "739"],
+    answer: "738",
+  },
+  {
+    question: "2 less than 321?",
+    options: ["319", "315", "324", "318"],
+    answer: "319",
+  },
+  {
+    question: "2 less than 50?",
+    options: ["48", "54", "46", "49"],
+    answer: "48",
+  },
+  {
+    question: "2 less than 78?",
+    options: ["76", "80", "74", "77"],
+    answer: "76",
+  },
+  {
+    question: "2 less than 195?",
+    options: ["193", "200", "192", "194"],
+    answer: "193",
+  },
+  {
+    question: "2 less than 17?",
+    options: ["15", "19", "13", "16"],
+    answer: "15",
+  },
+  {
+    question: "2 less than 621?",
+    options: ["619", "624", "615", "620"],
+    answer: "619",
+  },
+  {
+    question: "2 less than 384?",
+    options: ["382", "380", "390", "383"],
+    answer: "382",
+  },
+  {
+    question: "2 less than 267?",
+    options: ["265", "272", "260", "266"],
+    answer: "265",
+  },
+  {
+    question: "2 less than 142?",
+    options: ["140", "145", "138", "141"],
+    answer: "140",
+  },
+  {
+    question: "2 less than 331?",
+    options: ["329", "334", "326", "330"],
+    answer: "329",
+  },
+  {
+    question: "2 less than 75?",
+    options: ["73", "78", "70", "74"],
+    answer: "73",
+  },
+  {
+    question: "2 less than 62?",
+    options: ["60", "64", "58", "61"],
+    answer: "60",
+  },
+  {
+    question: "2 less than 912?",
+    options: ["910", "906", "916", "911"],
+    answer: "910",
+  },
+  {
+    question: "2 less than 847?",
+    options: ["845", "842", "852", "846"],
+    answer: "845",
+  },
+  {
+    question: "2 less than 162?",
+    options: ["160", "158", "168", "161"],
+    answer: "160",
+  },
+  {
+    question: "2 less than 533?",
+    options: ["531", "536", "528", "532"],
+    answer: "531",
+  },
+  {
+    question: "2 less than 93?",
+    options: ["91", "96", "88", "92"],
+    answer: "91",
+  },
+  {
+    question: "2 less than 671?",
+    options: ["669", "664", "674", "670"],
+    answer: "669",
+  },
+  {
+    question: "2 less than 228?",
+    options: ["226", "232", "224", "227"],
+    answer: "226",
+  },
+  {
+    question: "2 less than 768?",
+    options: ["766", "762", "772", "767"],
+    answer: "766",
+  },
+  {
+    question: "2 less than 410?",
+    options: ["408", "412", "404", "409"],
+    answer: "408",
+  },
+  {
+    question: "2 less than 135?",
+    options: ["133", "138", "130", "134"],
+    answer: "133",
+  },
+  {
+    question: "2 less than 893?",
+    options: ["891", "888", "898", "892"],
+    answer: "891",
+  },
+  {
+    question: "2 less than 357?",
+    options: ["355", "352", "362", "356"],
+    answer: "355",
+  },
+  {
+    question: "2 less than 44?",
+    options: ["42", "46", "40", "43"],
+    answer: "42",
+  },
+  {
+    question: "2 less than 576?",
+    options: ["574", "572", "582", "575"],
+    answer: "574",
+  },
+  {
+    question: "2 less than 213?",
+    options: ["211", "208", "218", "212"],
+    answer: "211",
+  },
+  {
+    question: "2 less than 817?",
+    options: ["815", "812", "822", "816"],
+    answer: "815",
+  },
+  {
+    question: "2 less than 741?",
+    options: ["739", "736", "746", "740"],
+    answer: "739",
+  },
     // Add more Level 2 questions...
   ];
   
--- QuestionBank/level3.js
+++ QuestionBank/level3.js
@@ -1,9 +1,204 @@
 const level3Questions = [
-    {
-        question: "Yet to load level 3 Question",
-        options: ["0", "0", "0", "0"],
-        answer: "B",
-      },
+     {
+    question: "Before 48?",
+    options: ["43", "47", "37", "38", "47"],
+    answer: "47",
+  },
+  {
+    question: "Before 27?",
+    options: ["24", "17", "18", "26", "26"],
+    answer: "26",
+  },
+  {
+    question: "Before 422?",
+    options: ["412", "413", "415", "421", "421"],
+    answer: "421",
+  },
+  {
+    question: "Before 483?",
+    options: ["473", "482", "475", "477", "482"],
+    answer: "482",
+  },
+  {
+    question: "Before 238?",
+    options: ["233", "235", "227", "237", "237"],
+    answer: "237",
+  },
+  {
+    question: "Before 70?",
+    options: ["64", "65", "69", "61", "69"],
+    answer: "69",
+  },
+  {
+    question: "Before 886?",
+    options: ["880", "885", "882", "875", "882"],
+    answer: "885",
+  },
+  {
+    question: "Before 13?",
+    options: ["9", "12", "3", "4", "12"],
+    answer: "12",
+  },
+  {
+    question: "Before 680?",
+    options: ["674", "679", "676", "669", "676"],
+    answer: "679",
+  },
+  {
+    question: "Before 72?",
+    options: ["61", "67", "69", "71", "71"],
+    answer: "71",
+  },
+  {
+    question: "Before 808?",
+    options: ["800", "807", "802", "804", "807"],
+    answer: "807",
+  },
+  {
+    question: "Before 84?",
+    options: ["75", "83", "77", "79", "79"],
+    answer: "83",
+  },
+  {
+    question: "Before 27?",
+    options: ["17", "26", "18", "21", "21"],
+    answer: "26",
+  },
+  {
+    question: "Before 36?",
+    options: ["33", "35", "28", "29", "29"],
+    answer: "35",
+  },
+  {
+    question: "Before 325?",
+    options: ["321", "324", "314", "316", "316"],
+    answer: "324",
+  },
+  {
+    question: "Before 265?",
+    options: ["260", "261", "264", "262", "264"],
+    answer: "264",
+  },
+  {
+    question: "Before 83?",
+    options: ["72", "82", "75", "79", "82"],
+    answer: "82",
+  },
+  {
+    question: "Before 39?",
+    options: ["38", "34", "29", "31", "31"],
+    answer: "38",
+  },
+  {
+    question: "Before 824?",
+    options: ["823", "820", "814", "815", "820"],
+    answer: "823",
+  },
+  {
+    question: "Before 874?",
+    options: ["873", "868", "870", "863", "870"],
+    answer: "873",
+  },
+  {
+    question: "Before 309?",
+    options: ["308", "307", "310", "305"],
+    answer: "308",
+  },
+  {
+    question: "Before 54?",
+    options: ["53", "52", "55", "51"],
+    answer: "53",
+  },
+  {
+    question: "Before 692?",
+    options: ["691", "690", "694", "688"],
+    answer: "691",
+  },
+  {
+    question: "Before 128?",
+    options: ["127", "126", "130", "122"],
+    answer: "127",
+  },
+  {
+    question: "Before 420?",
+    options: ["419", "418", "415", "416"],
+    answer: "419",
+  },
+  {
+    question: "Before 77?",
+    options: ["76", "75", "78", "74"],
+    answer: "76",
+  },
+  {
+    question: "Before 633?",
+    options: ["632", "631", "635", "630"],
+    answer: "632",
+  },
+  {
+    question: "Before 267?",
+    options: ["266", "265", "268", "263"],
+    answer: "266",
+  },
+  {
+    question: "Before 149?",
+    options: ["148", "147", "150", "146"],
+    answer: "148",
+  },
+  {
+    question: "Before 572?",
+    options: ["571", "570", "573", "569"],
+    answer: "571",
+  },
+  {
+    question: "Before 208?",
+    options: ["207", "206", "210", "205"],
+    answer: "207",
+  },
+  {
+    question: "Before 89?",
+    options: ["88", "87", "90", "86"],
+    answer: "88",
+  },
+  {
+    question: "Before 743?",
+    options: ["742", "741", "744", "740"],
+    answer: "742",
+  },
+  {
+    question: "Before 166?",
+    options: ["165", "164", "167", "163"],
+    answer: "165",
+  },
+  {
+    question: "Before 589?",
+    options: ["588", "587", "590", "586"],
+    answer: "588",
+  },
+  {
+    question: "Before 108?",
+    options: ["107", "106", "109", "105"],
+    answer: "107",
+  },
+  {
+    question: "Before 725?",
+    options: ["724", "723", "726", "722"],
+    answer: "724",
+  },
+  {
+    question: "Before 312?",
+    options: ["311", "310", "313", "309"],
+    answer: "311",
+  },
+  {
+    question: "Before 191?",
+    options: ["190", "189", "192", "188"],
+    answer: "190",
+  },
+  {
+    question: "Before 632?",
+    options: ["631", "630", "633", "629"],
+    answer: "631",
+  },
     // Add more Level 3 questions...
   ];
   
